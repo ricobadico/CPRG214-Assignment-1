9905a1d70e0c66c4131f03f80a588bf78af4d7fc
ricobadico <biondiceric@gmail.com>
2021-02-08 14:57:09 -0700

    fixing boat image

--- Ebiondic.CRPG214_A1.App/Controls/SlipView.ascx.cs
+++ Ebiondic.CRPG214_A1.App/Controls/SlipView.ascx.cs
@@ -15,16 +15,12 @@ namespace CPRG214.Assignment1.App.Controls
 
         }
 
-        // When created on the Slips page, this method is called with the given slip. 
-        // This renders the slip data in visual form according to this control
         public void fillSlipData(Slip slip)
         {
-            // Fill text boxes with data
             lblID.Text = $"Slip #{slip.ID}";
             lblSize.Text = $"{slip.Width}W x {slip.Length}L";
-            lblWater.Text = $"{(slip.Dock.WaterService ? "Water" : "-")}";
-            lblElectric.Text = $"{(slip.Dock.ElectricalService ? "Electrical" : "-")}";
-
+            lblWater.Text = $"{(slip.Dock.WaterService ? "yes" : "no")}";
+            lblElectric.Text = $"{(slip.Dock.ElectricalService ? "yes" : "no")}";
         }
     }
 }
